bc365fd89638ee54f08628eb668f9d8bb6e7b16d
hannahrajarao <hannahrajarao@gmail.com>
2023-05-03 01:32:07 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var questions = [
+    "What is the correct way to generate a random integer in the interval [a,b)?",
+
+]
+var answersChoices = [
+    ["Math.floor(Math.random()*b+a)", "Math.floor(Math.random()*(b-a)+a)", "Math.floor(Math.random()*a+b)", "Math.floor(Math.random()*b+a)"],
+]
+
+var correctAnswers = [1, ]
