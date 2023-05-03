2089c604e1d2a982927cce56cce141351cd76e5a
hannahrajarao <hannahrajarao@gmail.com>
2023-05-03 14:49:43 -0700

    Add questions and answer choices

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var questions = [
+    "Which of these is the correct way to print something to the console?",
+    "Which of these is a comment in JavaScript?",
+    "Which of these will not display an alert statement to the console",
     "What is the correct way to generate a random integer in the interval [a,b)?",
-
+    "Which of these functions will not return the value of a+b?",
 ]
 var answersChoices = [
-    ["Math.floor(Math.random()*b+a)", "Math.floor(Math.random()*(b-a)+a)", "Math.floor(Math.random()*a+b)", "Math.floor(Math.random()*b+a)"],
+    ["window.print()", "console.print()", "window.log()", "console.log()"],
+    ["// comment", "<!--comment-->", "/*comment*/", "# comment"],
+    ["alert", "confirm", "prompt", "print"],
+    ["Math.floor(Math.random()*b+a)", "Math.floor(Math.random()*(b-a)+a)", "Math.floor(Math.random()*a+b)", "Math.floor(Math.random()*(a-b)+a)"],
+    ["var add = (a, b)=> a+b;", "function add(a, b) {return a+b}", "function(a, b) {a+b}","var add = function(a, b) {return a+b}"]
 ]
 
-var correctAnswers = [1, ]
+var correctAnswers = [3, 0, 3, 1, 2];
